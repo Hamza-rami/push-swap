1887b6f32aa6e79f42b2be49b3977d28b47abcef
Hamza Rami <hrami@e1r4p13.1337.ma>
2025-01-01 17:46:21 +0100

    update

---
+++ lisub.c
@@ -0,0 +1,47 @@
+#include "push_swap.h"
+
+
+int *find_lis(int *arr, int size, int *lis_size) {
+    if (size == 0) {
+        *lis_size = 0;
+        return NULL;
+    }
+
+    int *lis_length = malloc(size * sizeof(int));
+    int *prev = malloc(size * sizeof(int));
+    int i, j;
+
+    for (i = 0; i < size; i++) {
+        lis_length[i] = 1;
+        prev[i] = -1;
+    }
+
+    for (i = 1; i < size; i++) {
+        for (j = 0; j < i; j++) {
+            if (arr[j] < arr[i] && lis_length[j] + 1 > lis_length[i]) {
+                lis_length[i] = lis_length[j] + 1;
+                prev[i] = j;
+            }
+        }
+    }
+
+    int max_index = 0;
+    for (i = 1; i < size; i++) {
+        if (lis_length[i] > lis_length[max_index])
+            max_index = i;
+    }
+
+    *lis_size = lis_length[max_index];
+
+    int *lis = malloc((*lis_size) * sizeof(int));
+    int idx = *lis_size - 1;
+    while (max_index != -1) {
+        lis[idx--] = arr[max_index];
+        max_index = prev[max_index];
+    }
+
+    free(lis_length);
+    free(prev);
+
+    return lis;
+}
